acddd09622404609344d26b4276e4e9895e84eae
Poliana <poliana.santos.queiroz@gmail.com>
2019-11-22 09:05:54 -0300

    Segunda versão

--- jogo.py
+++ jogo.py
@@ -1,4 +1,5 @@
 import random
+import time
 from pygame import mixer
 
 #lista que contem as perguntas do jogo
@@ -12,8 +13,12 @@ nomeDeUsuario = ''
 def exibirRanking(lista):
     ran = open('rankingOrdenado.txt','w', encoding='latin-1')
     print('\nVeja qual foi a sua posição no nosso Ranking, Jovem Padawan!\n')
+    print('\nCarregando posições no ranking...\n')
+    time.sleep(3)
+    print('\nSe você estiver entre os 3 primeiros você se tornou um JEDI!!\n')
+    time.sleep(3)
     for i in lista:
-        print (i)
+        print ('\n',i)
         ran.write(i + '\n')
     
 
@@ -32,7 +37,7 @@ def ordenaRanking():
                 if int(lista[i]) > maior:
                     maior = int(lista[i])
                     indice = i
-        listaOrdenados.append(lista[indice-1]  + '-' +  lista[indice] ) 
+        listaOrdenados.append(lista[indice-1]  + ' - ' +  lista[indice] ) 
         del lista[indice]
         del lista[indice-1]
     exibirRanking(listaOrdenados)
@@ -43,18 +48,39 @@ def musica():
     mixer.music.load('musica.mp3')
     mixer.music.play()
 
+#função para tocar musica quando o usuário acertar
+def musica_errou():
+    mixer.init()
+    mixer.music.load('Errou.mp3')
+    mixer.music.play()
+
+#função paratocar musica quando acertar
+def musica_acertou():
+    mixer.init()
+    mixer.music.load('acertoumiseravel.mp3')
+    mixer.music.play()
+
 #função que exibi o as boas vindas ao usuário
 def exibir():
     print('\n================================================================================\n')
-    print('\n                     Seja bem vindo jovem padawan!!                             \n')
+    print('\n                            Atenção!!!                                          \n')
+    print('\n          ***Jogo altamente recomendado para fãs de Star Wars***                \n')
     print('\nResponda as perguntas corretamente e saiba se você pode se tornar um grande Jedi!\n')
     print('\n================================================================================\n')
+    time.sleep(7)
+    print('\n                    Seja bem vindo jovem padawan!! :D                           \n')
+    print('                             \U0001F604', '\U0001F604', '\U0001F604'                 )
+    print('\n================================================================================\n')
+    print('\n                           Preparado está?                                      \n')
+    print('\n================================================================================\n')
+    time.sleep(3)
 
 #função que pega o nome do usuário
 def cadastrar():
     global nomeDeUsuario
     print('\n================================================================================\n')
     nomeDeUsuario = input('\nDigite Seu nome jovem Padawan: ').upper()
+    print('\nPrepare-se para a primeira pergunta',nomeDeUsuario,'que a força esteja com você!\n')
     print('\n================================================================================\n')
 
 #função que cria o ranking
@@ -68,6 +94,7 @@ def criarRanking ():
 #função que exibi as perguntas para o usuário e compara a sua resposta
 def pergunta(str):
     print('\n===============================================================================\n')
+    time.sleep(2)
     arquivo = open(str, 'r',encoding='latin-1')
     arquivo = arquivo.readlines()
     global pontos #para que a função consiga enxergar a várial pontos
@@ -77,24 +104,28 @@ def pergunta(str):
             print(linhas)
         if posiçao == 4:
             resposta = input('Digite sua resposta: \n' ).upper()
-            #tratamento de erros
+            #pequeno tratamento de erros
             while resposta != 'A' and resposta != 'B' and resposta != 'C' and resposta != 'D':
                 print('\nResposta inválida jovem padawan, tente novamente\n')
                 resposta = input('Digite sua resposta: \n' ).upper()
 
         if posiçao == 5:
             if resposta in linhas:
-                pontos += 10
-                print('\nParabéns Jovem Padawan, VOCÊ ACERTOU!!!\n')
+                pontos += 100
+                musica_acertou()
+                print('\nBOAAAAAA Jovem Padawan, VOCÊ ACERTOU!!!\n')
+                time.sleep(3)
+                musica()
+                
             elif resposta not in linhas:
-                print('\nJovem Padawan, Você errou!!\n')
-                print("\nA resposta correta era:")
-                print(linhas)
-    acabou()
-        
-
-
-    
+                musica_errou()
+                print('\n Que triste Jovem Padawan :(, Você errou!!\n')
+                print("\nA resposta correta era:",linhas)
+                #print(linhas)
+                time.sleep(3)
+                musica()
+                
+    acabou()  
 
 #função que sorteia a pergunta que será exibida para o usuário e pergunta o mesmo deseja continuar jogando
 def excutar():
@@ -106,18 +137,23 @@ def excutar():
         #vai chmar a função pergunta exibir na tela
         pergunta(nome)
 
-        sair = input('Digite "C" para continuar e "S" para sair: \n').upper()
+        sair = input('Digite "ENTER" para continuar e "S" para sair: \n').upper()
         if sair == 'S':
             break
     
-        print('\nSeu total de pontos é:', pontos)
+        print('\nSeu total de pontos até o momento é:', pontos)
 
 #para o jogo quando as perguntas acabam
 def acabou():
     if len(lista) == 0:
         pass
-    
 
+#despedida do usuário
+def adeus():
+    print(                 '\nAté mais grande jedi!\n'                   )
+    time.sleep(3)
+    print(             '\n***Que a força esteja com você!***\n'              )
+    print('\U0001F590','\U0001F590','\U0001F590','\U0001F590','\U0001F590','\U0001F590')
 
 
 musica()
@@ -131,4 +167,8 @@ excutar()
 criarRanking()
 
 ordenaRanking()
+
+adeus()
+
+
 
--- jotf.py
+++ jotf.py
@@ -1,13 +1,23 @@
 from tkinter import* 
 
+def exibir():
+    lab['text'] = 'Responda as perguntas corretamente e saiba se você pode se tornar um grande Jedi'
+    
+
 janela = Tk()
 janela.title('Quizz Star Wars')
 janela['bg'] = 'blue' #cor da janela
 
 janela.geometry('800x600+100+50')
 
-lab = Label(janela, text = '  Seja bem vindo jovem padawan!!  ')
-lab.place(x=250, y=250)
+lab = Label(janela, text = 'Seja bem vindo jovem padawan!!')
+lab.place(x=250, y=250) #dimenssões do texto na janela
+
+#criando botão na tela
+bt = Button(janela, width = 20, text = 'continuar', command =  exibir)
+bt.place(x=280, y=450) #dimenssões do botão na janela
+
+
 
 #a janela fica aberta até que o usuário a feche
 janela.mainloop()
